3e3a31711b1eb32c6f3bfefef26bb14ac4baf309
matg0mes <hotmus8500@gmail.com>
2022-09-19 16:01:56 -0300

    feat(index): add tests

--- src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -1,13 +1,12 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 public class IndexController {
-
-    public String index(){
-
+    public String index() {
         return "index";
     }
 
-    public String oupsHandler(){
+    public String oupsHandler() {
         return "notimplemented";
     }
+
 }
