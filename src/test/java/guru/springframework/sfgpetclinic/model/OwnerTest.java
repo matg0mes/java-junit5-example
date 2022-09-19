f679607dd33960f651cb124c6e6ef870df3dc412
matg0mes <hotmus8500@gmail.com>
2022-09-19 17:25:13 -0300

    feat(owner): add tests

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,27 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+    @Test
+    void dependentAssertions() {
+        Owner owner = new Owner(1L, "Joe", "Buck");
+        owner.setCity("Key West");
+        owner.setTelephone("12345678");
+
+        assertAll("Properties Test",
+                () -> assertAll("Person Properties",
+                        () -> assertEquals("Joe", owner.getFirstName(), "First Name Did Not Match"),
+                        () -> assertEquals("Buck", owner.getLastName(), "Last Name Did Not Match")
+                ),
+                () -> assertAll("Owner Properties",
+                        () -> assertEquals("Key West", owner.getCity(), "City Dis Not Match"),
+                        () -> assertEquals("12345678", owner.getTelephone(), "Telephone Did Not Match")
+                )
+        );
+    }
+
+}
