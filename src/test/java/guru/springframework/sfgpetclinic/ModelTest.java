b82cc12b1a258b02ca6cfd34455d7d3cdfa824ea
matg0mes <hotmus8500@gmail.com>
2022-09-20 11:26:34 -0300

    feat(tests): dependency injection

--- src/test/java/guru/springframework/sfgpetclinic/ModelTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/ModelTest.java
@@ -1,7 +1,17 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInfo;
 
 @Tag("model")
 public interface ModelTest {
+
+//    @BeforeEach
+//    default void beforeEachConsoleOutputer(TestInfo info, RepetitionInfo repetitionInfo) {
+//        System.out.println("Running Test - " + info.getDisplayName() + " - "
+//                + repetitionInfo.getCurrentRepetition() + " | " + repetitionInfo.getTotalRepetitions());
+//    }
+
 }
--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,9 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTest;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,4 +30,23 @@ class PersonTest implements ModelTest {
                 () -> assertEquals("Buck", person.getLastName(), "Last Name Failed")
         );
     }
+
+    @RepeatedTest(value = 10, name = RepeatedTest.LONG_DISPLAY_NAME)
+    @DisplayName("My repeated Test")
+    void myRepeatedTest() {
+        //todo - impl
+    }
+
+    @RepeatedTest(value = 5)
+    @DisplayName("My repeated Test")
+    void myRepeatedTestWithId(TestInfo info, RepetitionInfo repetitionInfo) {
+        System.out.println(info.getDisplayName() + ": " + repetitionInfo.getCurrentRepetition());
+    }
+
+    @RepeatedTest(value = 5, name = RepeatedTest.LONG_DISPLAY_NAME)
+    @DisplayName("My repeated Test")
+    void myAssignmentRepeat(TestInfo info, RepetitionInfo repetitionInfo) {
+
+    }
+
 }
