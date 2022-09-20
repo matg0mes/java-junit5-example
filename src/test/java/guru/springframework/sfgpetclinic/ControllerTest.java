338e4ce952d00dfe2808cd4515193198cd5c7ce2
matg0mes <hotmus8500@gmail.com>
2022-09-20 10:55:59 -0300

    feat(tests): before all default per class

--- src/test/java/guru/springframework/sfgpetclinic/ControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/ControllerTest.java
@@ -1,7 +1,14 @@
 package guru.springframework.sfgpetclinic;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
-@Tag("controller")
+@Tag("controllers")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public interface ControllerTest {
+    @BeforeAll
+    default void beforeAll() {
+        System.out.println("Lets do somethiing here");
+    }
 }
