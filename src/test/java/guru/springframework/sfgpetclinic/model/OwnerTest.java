cd44e7d192820a5f5aa6bfb298c11ca0d3dea0e8
matg0mes <hotmus8500@gmail.com>
2022-09-20 22:43:30 -0300

    feat(owner): add csv tests

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -5,10 +5,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -64,4 +63,18 @@ class OwnerTest implements ModelTest {
         System.out.println(stateName + " = " + val1 + ":" + val2);
     }
 
+    @DisplayName("Method Provider Test - ")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @MethodSource("getArgs")
+    void fromMethodTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " = " + val1 + ":" + val2);
+    }
+
+    static Stream<Arguments> getArgs() {
+        return Stream.of(
+                Arguments.of("FL", 1, 1),
+                Arguments.of("OH", 1, 1),
+                Arguments.of("MI", 1, 1)
+        );
+    }
 }
