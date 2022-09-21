d27905da3615581db5453fb6c9bee43d21c88fd0
matg0mes <hotmus8500@gmail.com>
2022-09-20 22:26:40 -0300

    feat(owner): add parametrized tests

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,8 +1,12 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,4 +30,17 @@ class OwnerTest implements ModelTest {
         );
     }
 
+    @DisplayName("Value Source Test - ")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @ValueSource(strings = {"Spring", "Framework"})
+    void testValueSource(String val) {
+        System.out.println(val);
+    }
+
+    @DisplayName("Enum Source Test - ")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @EnumSource(OwnerType.class)
+    void enumTest(OwnerType ownerType) {
+        System.out.println(ownerType);
+    }
 }
